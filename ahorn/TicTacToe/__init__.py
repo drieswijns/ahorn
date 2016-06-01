e624ddd5cf30857900a9df60aba0256d7af45220
Dries Wijns <dries.wijns@gmail.com>
2016-06-01 22:31:45 +0200

    Updated Tic Tac Toe docstring

--- ahorn/TicTacToe/__init__.py
+++ ahorn/TicTacToe/__init__.py
@@ -1,2 +1,10 @@
+"""Tic-Tac-Toe is the simpelest of games.
+
+The game is deterministic: there are no dice or random cards present.
+The game is of complete information: every players knows the board.
+
+This package servers as an example on how to implement simple games.
+"""
+
 from .TTTActions import TTTAction
 from .TTTStates import TTTState
