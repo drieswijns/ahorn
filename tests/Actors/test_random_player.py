83cbb6eafb1e1e3abee4171795e141c34d684e10
Dries Wijns <dries.wijns@gmail.com>
2016-06-01 22:06:08 +0200

    Description change

--- tests/Actors/test_random_player.py
+++ tests/Actors/test_random_player.py
@@ -19,9 +19,9 @@ def test_random_player():
 
     chosen_actions = set()
     for _ in range(100):
-        # After 100 we expect that all legal actions should be chosen_action
-        # at least once by the random player
+        # After 100 samples we expect that all legal actions should be
+        # chosen at least once by the random player
         chosen_action = rp.get_action(state)
         chosen_actions.add(hash(chosen_action))
-    
+
     assert(len(all_actions^chosen_actions) == 0)
