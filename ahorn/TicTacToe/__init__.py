90020611380b614c2d25d0968af286b0ad65ba28
Dries Wijns <dries.wijns@gmail.com>
2016-08-01 08:58:49 +0200

    Made tic-tac-toe module docstring more english like

--- ahorn/TicTacToe/__init__.py
+++ ahorn/TicTacToe/__init__.py
@@ -3,7 +3,7 @@
 The game is deterministic: there are no dice or random cards present.
 The game is of complete information: every players knows the board.
 
-This package servers as an example on how to implement simple games.
+This module serves as an example on how to implement simple games.
 """
 
 from .Actions import TicTacToeAction
