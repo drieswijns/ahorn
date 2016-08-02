83dc9fa7df1e06ff8dd3546b520e3785b080d809
Dries Wijns <dries.wijns@gmail.com>
2016-08-02 16:09:07 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'ahorn',
   packages = ['ahorn', 'ahorn.Actors', 'ahorn.GameBase', 'ahorn.TicTacToe'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'A game playing and game AI library',
   author = 'Dries Wijns',
   author_email = 'dries.wijns@gmail.com',
